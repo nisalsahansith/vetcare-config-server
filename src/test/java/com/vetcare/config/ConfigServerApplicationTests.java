a6375d615b61be326ef77b083b619bc641a9d84d
nisalsahansith <nisalsahansith@gmail.com>
2026-08-17 17:05:27 +0530

    creat config  service

---
+++ src/test/java/com/vetcare/config/ConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vetcare.config;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
